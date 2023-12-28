527a495f72f4a2913de14222931196d3449ccbae
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2023-12-28 10:26:32 +0530

    Create dict.py

---
+++ Language Fundamentals/28-12-2023/dict.py
@@ -0,0 +1,6 @@
+d={} #empty dict
+d={1:'kalyan',2:'python',3:'java'} # key value pairs can be any object
+type(d) # <class 'dict'>
+d[100]='sql' # by default dict is mutalbe
+d[101]='bunny'
+d[100] = 'sunny' #sql is replaced with sunny 

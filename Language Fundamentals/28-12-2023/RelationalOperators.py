e4aa2185500d28bd5e681c58c9ae86738e8f480e
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2023-12-28 15:41:04 +0530

    Create RelationalOperators.py

---
+++ Language Fundamentals/28-12-2023/RelationalOperators.py
@@ -0,0 +1,28 @@
+#Relational operators on integers
+a=10 b=20 
+print("a > b is ",a>b) #False
+print("a >= b is ",a>=b) #False
+print("a < b is ",a<b) #True
+print("a <= b is ",a<=b) #True
+
+# Relational operator on strings
+
+a="durga" 
+b="durga" 
+print("a > b is ",a>b) #False
+print("a >= b is ",a>=b) #True
+print("a < b is ",a<b) #False
+print("a <= b is ",a<=b) #True
+
+#Relational Operators on Boolean
+
+print(True>True) #False 
+print(True>=True) #True 
+print(10 >True) #True 
+print(False > True) #False
+
+#Chaining of relational operators
+10<20 ==>True 
+10<20<30 ==>True 
+10<20<30<40 ==>True 
+10<20<30<40>50 ==>False

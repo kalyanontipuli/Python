9bfa1270b862f414dd26bf3b750a5c9f582a4f6c
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2023-12-28 08:48:23 +0530

    Create tuple.py

---
+++ Language Fundamentals/28-12-2023/tuple.py
@@ -0,0 +1,6 @@
+t=(10,'kalyan',True,10,[50,60]) #duplicates are allowed also list is allowed
+print(type(t)) 
+print(t[0]) # index operator is allowed
+print(t[0:5]) # slicing is allowed in tuple
+#t[0]=100 #type Error: tuple is immutable
+

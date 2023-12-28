29dbff89da49f155bf0df5438f2552aaa78b5bba
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2023-12-28 15:07:17 +0530

    Create ArithmeticOperators.py

---
+++ Language Fundamentals/28-12-2023/ArithmeticOperators.py
@@ -0,0 +1,9 @@
+a=10 
+b=2 
+print('a+b=',a+b) #12
+print('a-b=',a-b) #8
+print('a*b=',a*b) #20
+print('a/b=',a/b) #5.0 '/' operators always gives float as output
+print('a//b=',a//b) #5
+print('a%b=',a%b) #0
+print('a**b=',a**b) #100

8b6eba7704caca79dba18f2a51a9cad5dec04ea8
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2023-12-28 09:19:18 +0530

    Create range.py

---
+++ Language Fundamentals/28-12-2023/range.py
@@ -0,0 +1,15 @@
+# Form-1
+r=range(10) # to represent numbers from 0 to 9
+print(type(r)) # <class 'range'>
+for i in r:
+    print(i) # 0 to 9 gets printed
+r[4]=4 # indexing is allowed
+
+#Form - 2
+r= range(20,30) # To represent numbers from 20 to 29 
+for i in r:
+    print(i)
+# form - 3
+r= range(5,50,5) # range is from 5 to 49 with step value 5
+for i in r:
+    print(i)

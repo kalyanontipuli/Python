c1f37bc829e5ee6cf8a2d66b1eff0c3351323fd4
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2023-12-28 09:47:01 +0530

    Create set.py

---
+++ Language Fundamentals/28-12-2023/set.py
@@ -0,0 +1,4 @@
+s={10,20,30,10,20} 
+print(s) # ouput will be 10 20 30 bcz duplicates are not allowed in set
+print(s[0]) # type error:indexing is not maintained in set
+s.add('kalyan') # Heterogenous data is allowed in set

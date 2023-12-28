9734e258a1b136567f0240387999d4ef02ea1b4b
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2023-12-28 17:16:46 +0530

    Create LogicalOperators.py

---
+++ Language Fundamentals/28-12-2023/LogicalOperators.py
@@ -0,0 +1,25 @@
+# for boolean data type
+
+True and False # False
+True or False #True
+not False #True
+
+# For non boolean data types
+
+10 and 20 # 20
+0 and 10 # 0
+10 and 0 # 0
+'durga' and '' # ''
+'' and 'durga' # ''
+
+10 or 20 # 10
+0 or 20 # 20
+20 or 0 # 20
+o or o # 0
+'' or 'durga' # 'durga'
+'durga' or '' # 'durga'
+
+not 10 # Falswe
+not '' # True
+
+10 or 10/0 # True

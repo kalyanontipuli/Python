66198fa8b3ff75ba11b27c9ad1df9b56325888e4
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2023-12-29 15:54:57 +0530

    Create MembershipOperator.py

---
+++ Language Fundamentals/29-12-2023/MembershipOperator.py
@@ -0,0 +1,10 @@
+x="hello learning Python is very easy!!!" 
+print('h' in x) #True 
+print('d' in x) #False 
+print('d' not in x) #True 
+print('Python' in x) #True
+
+list1=["sunny","bunny","chinny","pinny"] 
+print("sunny" in list1) #True 
+print("tunny" in list1) #False 
+print("tunny" not in list1) #True

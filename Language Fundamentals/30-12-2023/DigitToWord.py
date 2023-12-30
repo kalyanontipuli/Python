e7626f73c33f3a60c1af649c137a0a5de0623961
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2023-12-30 17:30:49 +0530

    Create DigitToWord.py

---
+++ Language Fundamentals/30-12-2023/DigitToWord.py
@@ -0,0 +1,24 @@
+#Write a program to take a single digit number from the key board and print is value in English word
+n=int(input("Enter a digit from o to 9:")) 
+if n==0 :
+  print("ZERO") 
+elif n==1:
+  print("ONE") 
+elif n==2:
+  print("TWO")
+elif n==3:
+  print("THREE") 
+elif n==4:
+  print("FOUR") 
+elif n==5: 
+  print("FIVE") 
+elif n==6: 
+  print("SIX") 
+elif n==7: 
+  print("SEVEN") 
+elif n==8: 
+  print("EIGHT") 
+elif n==9: 
+  print("NINE") 
+else: 
+  print("PLEASE ENTER A DIGIT FROM 0 TO 9")

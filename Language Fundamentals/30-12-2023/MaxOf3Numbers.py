afa12b3673a6615263a16dbe0601723db7b27892
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2023-12-30 17:26:48 +0530

    Create MaxOf3Numbers.py

---
+++ Language Fundamentals/30-12-2023/MaxOf3Numbers.py
@@ -0,0 +1,7 @@
+# Write a program to find biggest of given 2 numbers from the commad prompt
+n1=int(input("Enter First Number:")) 
+n2=int(input("Enter Second Number:")) 
+if n1>n2:
+  print("Biggest Number is:",n1) 
+else : 
+  print("Biggest Number is:",n2)

130a479d9e0b02a487f2eeb91f7fbed943594959
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2024-01-03 16:46:23 +0530

    Create changingCase.py

---
+++ Language Fundamentals/3-1-2024/changingCase.py
@@ -0,0 +1,8 @@
+s='learning Python is very Easy'  
+print(s.upper()) # LEARNING PYTHON IS VERY EASY
+print(s.lower()) #learning python is very easy
+print(s.swapcase()) #LEARNING pYTHON IS VERY eASY
+print(s.title()) # Learning Python Is Very Easy
+print(s.capitalize()) # Learning python is very easy
+
+

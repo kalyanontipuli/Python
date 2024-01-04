bfd5c8ef6b85f2f42cc63414392c3f0de4b23291
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2024-01-04 17:15:19 +0530

    Create accessingList.py

---
+++ Language Fundamentals/04-01-2024/accessingList.py
@@ -0,0 +1,13 @@
+# using index operator
+list=[10,20,30,40]
+print(list[0]) ==>10
+print(list[-1]) ==>40
+
+#using slice operator
+
+n=[1,2,3,4,5,6,7,8,9,10] 
+print(n[2:7:2]) 
+print(n[4::2]) 
+print(n[3:7]) 
+print(n[8:2:-2]) 
+print(n[4:100])

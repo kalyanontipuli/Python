cd0c145622891d68fe3c86217b79d28f130c1f80
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2024-01-04 17:12:48 +0530

    Create listCreation.py

---
+++ Language Fundamentals/04-01-2024/listCreation.py
@@ -0,0 +1,8 @@
+list=[10,20,30,40] # If we know elements
+list=eval(input("Enter List:")) # with dynamic input
+l=list(range(0,10,2)) # using list function
+
+s="Learning Python is very very easy !!!" 
+l=s.split() #using split function
+
+

79655b6924fc50aa70cc1565398dfde8e875dcf8
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2024-01-04 17:22:54 +0530

    Create ManipulatingList.py

---
+++ Language Fundamentals/04-01-2024/ManipulatingList.py
@@ -0,0 +1,26 @@
+#append() function
+list=[] 
+list.append("A") 
+list.append("B") 
+list.append("C")
+print(l) # ['A', 'B', 'C']
+
+#insert() function
+n=[1,2,3,4,5] 
+n.insert(1,888) 
+print(n)
+
+#extend() function
+
+order1=["Chicken","Mutton","Fish"] 
+order2=["RC","KF","FO"] 
+order1.extend(order2)
+
+#remove() function
+n=[10,20,10,30] 
+2) n.remove(10)
+
+#pop() function
+
+n=[10,20,30,40] 
+print(n.pop())

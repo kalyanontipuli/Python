bda209da3ce7a58b7b0d4bc7ba691ffa38bf8437
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2024-01-04 17:19:03 +0530

    Create ListFunctions.py

---
+++ Language Fundamentals/04-01-2024/ListFunctions.py
@@ -0,0 +1,10 @@
+n=[10,20,30,40]
+print(len(n))==>4  #len() function
+
+n=[1,2,2,2,2,3,3] 
+print(n.count(1)) ==>1  count() function
+print(n.count(2)) ==>4
+
+n=[1,2,2,2,2,3,3] 
+print(n.index(1)) ==>0 #index() function
+print(n.index(2)) ==>1 

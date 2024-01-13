92fb3fb7ef36de2300b240e2ebd7afa77a4da38d
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2024-01-13 17:41:29 +0530

    Create PickleReadingAndWriting.py

---
+++ Language Fundamentals/13-1-2024/PickleReadingAndWriting.py
@@ -0,0 +1,18 @@
+import pickle 
+class Employee:
+  def __init__(self,eno,ename,esal,eaddr): 
+    self.eno=eno; 
+    self.ename=ename; 
+    self.esal=esal; 
+    self.eaddr=eaddr; 
+  def display(self):
+    print(self.eno,"\t",self.ename,"\t",self.esal,"\t",self.eaddr) 
+with open("emp.dat","wb") as f: 
+  e=Employee(100,"Durga",1000,"Hyd")
+  pickle.dump(e,f)
+  print("Pickling of Employee Object completed...")
+  
+with open("emp.dat","rb") as f: 
+  obj=pickle.load(f) 
+  print("Printing Employee Information after unpickling") 
+  obj.display()

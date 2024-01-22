814b1f11c318c32593dce2dbc9a6539422388b52
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2024-01-22 17:45:34 +0530

    Create Divisible and Non-divisible Sums Difference.py

---
+++ LeetCode/Divisible and Non-divisible Sums Difference.py
@@ -0,0 +1,10 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        sum1,sum2=0,0
+        for i in range(1,n+1):
+            if i%m!=0:
+                sum1=sum1+i
+            else:
+                sum2=sum2+i
+        return sum1-sum2
+        

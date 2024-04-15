c9eccaca42002b27cdb012994525f07c8385f49a
kalyanontipuli <126324034+kalyanontipuli@users.noreply.github.com>
2024-04-15 17:25:02 +0530

    Update database.py

--- Tasks/FastAPI task/Database/database.py
+++ Tasks/FastAPI task/Database/database.py
@@ -1,15 +1,13 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from config import DB_PASSWORD,DB_USERNAME
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:root@localhost/FreeResources'
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@localhost/FreeResources'
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
-
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
